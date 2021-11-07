9797187a17500293f3d3db594a2236151ea8f98b
elbossesito <80911694+elbossesito@users.noreply.github.com>
2021-11-07 21:53:37 +0100

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,3 +1,2 @@
 @echo off
-TITLE I-BASE 2k21 V1.2
-C:\dlls\FXServer.exe +exec  C:\Users\xxike\Desktop\I-BASE-V1-FINAL\data\server.cfg +set onesync legacy
+C:\users\administrador\FXServer.exe +exec  C:\Users\administrador\Desktop\BASEESX\data\server.cfg +set onesync legacy
